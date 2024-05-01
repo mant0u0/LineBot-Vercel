30931c85909a17b5043df48f22c449a9b46bdf7c
mant0u0 <mant0u.goo@gmail.com>
2024-05-01 20:17:43 +0800

    [Mantou] linebot.v3 SDK 測試

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
+from linebot.v3 import LineBotApi, WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.models import *
 
 import os
 import json
